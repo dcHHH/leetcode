eed1060fc26797cdedd3122b93becae3d8cd5abd
dcHHH <744947370@QQ.COM>
2017-08-04 15:20:34 +0800

    add 136, 190, 191, 344, 461

--- Single Number.py
+++
@@ -1,8 +0,0 @@
-#Given an array of integers, every element appears twice \
-#except for one. Find that single one.
-#Your algorithm should have a linear runtime complexity
-
-
-class Solution(object):
-    def singleNumber(self, nums):
-        return reduce(lambda x, y: x ^ y, nums)        
