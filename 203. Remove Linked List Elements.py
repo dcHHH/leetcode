1aacfd08aec69653e30efefec2040589ae63f89a
dcHHH <744947370@QQ.COM>
2017-09-12 19:25:34 +0800

    add 101,198,203,206,237,387,70

---
+++ 203. Remove Linked List Elements.py
@@ -0,0 +1,32 @@
+'''
+Remove all elements from a linked list of integers that have value val.
+
+Example
+Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+Return: 1 --> 2 --> 3 --> 4 --> 5
+'''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        if not head or not val:
+            return head
+        node = ListNode(None)
+        node.next = head
+        ans = node
+        while node.next:
+            if node.next.val == val:
+                node.next = node.next.next
+            else:
+                node = node.next
+        return ans.next
---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,21 @@
+'''
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+'''
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        while n < 2:
+            return n
+        a, b = 0, 1
+        while n > 0:
+            a, b = b, a + b
+            n -= 1
+        return b
