615948efdc112ca5d97fd2af20a11c66e18e7ff8
dcHHH <744947370@QQ.COM>
2017-09-17 17:04:43 +0800

    add 110,232,235,513,515

---
+++ 515. Find Largest Value in Each Tree Row.py
@@ -0,0 +1,33 @@
+'''
+You need to find the largest value in each row of a binary tree.
+
+Example:
+Input: 
+
+          1
+         / \
+        3   2
+       / \   \  
+      5   3   9 
+
+Output: [1, 3, 9]
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def largestValues(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        ans, level = [], [root]
+        while root and level:
+            ans.append(max([i.val for i in level]))
+            level = [kid for i in level for kid in (i.left, i.right) if kid]
+        return ans
