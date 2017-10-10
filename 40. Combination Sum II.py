c3d7ccc83d94f57c4299354c3d6cd93f2133f644
dcHHH <744947370@QQ.COM>
2017-10-10 11:30:03 +0800

    add 40

---
+++ 40. Combination Sum II.py
@@ -0,0 +1,39 @@
+'''
+Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+Each number in C may only be used once in the combination.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+For example, given candidate set [10, 1, 2, 7, 6, 1, 5] and target 8, 
+A solution set is: 
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+'''
+
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        def dfs(nums, target, index, path, ans):
+            if target < 0:
+                return
+            elif target == 0:
+                ans.append(path)
+                return
+            for i in range(index, len(nums)):
+                if not (i > index and nums[i] == nums[i - 1]):
+                    dfs(nums, target - nums[i], i + 1, path + [nums[i]], ans)
+        
+        candidates.sort()
+        ans = []
+        dfs(candidates, target, 0, [], ans)
+        return ans
